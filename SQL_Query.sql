479da9e2e28b05a7ed874bb0e77a4aa8d69e322a
Avinash Singh <awsingh7@gmail.com>
2025-02-16 10:31:33 +0530

    Update SQL_Query.sql

--- SQL_Query.sql
+++ SQL_Query.sql
@@ -1,21 +1,22 @@
--- SQL Retail Sales Analysis - P1
-CREATE DATABASE sql_project_p2;
+-- SQL Retail Sales Analysis --
 
+CREATE DATABASE retail;
 
--- Create TABLE
+-- Create TABLE --
+
 DROP TABLE IF EXISTS retail_sales;
 CREATE TABLE retail_sales
             (
                 transaction_id INT PRIMARY KEY,	
                 sale_date DATE,	 
                 sale_time TIME,	
-                customer_id	INT,
-                gender	VARCHAR(15),
-                age	INT,
+                customer_id INT,
+                gender VARCHAR(15),
+                age INT,
                 category VARCHAR(15),	
-                quantity	INT,
+                quantity INT,
                 price_per_unit FLOAT,	
-                cogs	FLOAT,
+                cogs FLOAT,
                 total_sale FLOAT
             );
 
@@ -23,22 +24,11 @@ SELECT * FROM retail_sales
 LIMIT 10
 
 
-    
-
-SELECT 
-    COUNT(*) 
+SELECT  COUNT(*) 
 FROM retail_sales
 
--- Data Cleaning
-SELECT * FROM retail_sales
-WHERE transactions_id IS NULL
-
-SELECT * FROM retail_sales
-WHERE sale_date IS NULL
-
-SELECT * FROM retail_sales
-WHERE sale_time IS NULL
-
+-- Data Cleaning --
+            
 SELECT * FROM retail_sales
 WHERE 
     transaction_id IS NULL
@@ -76,7 +66,7 @@ WHERE
     OR
     total_sale IS NULL;
     
--- Data Exploration
+-- Data Exploration --
 
 -- How many sales we have?
 SELECT COUNT(*) as total_sale FROM retail_sales
@@ -86,14 +76,13 @@ SELECT COUNT(*) as total_sale FROM retail_sales
 SELECT COUNT(DISTINCT customer_id) as total_sale FROM retail_sales
 
 
-
 SELECT DISTINCT category FROM retail_sales
 
 
 -- Data Analysis & Business Key Problems & Answers
 
 -- My Analysis & Findings
--- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
+-- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05'
 -- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 10 in the month of Nov-2022
 -- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
 -- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
@@ -106,7 +95,7 @@ SELECT DISTINCT category FROM retail_sales
 
 
 
- -- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
+ -- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05'
 
 SELECT *
 FROM retail_sales
